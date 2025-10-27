ce2521a1c81df2c743ac547914808ee1c17afc2e
iphuck22 <eldeshue@naver.com>
2025-10-27 14:09:40 +0900

    test : add test for PhysicalDeviceCreation

--- tests/RhiTest.cc
+++ tests/RhiTest.cc
@@ -7,3 +7,10 @@ TEST(RhiInstance, InstanceCreation) {
         GraphRunner::Rhi::Instance("Rhi Test build", "GraphRunner", { }, { });
     });
 }
+
+TEST(RhiPhysicalDevice, PhysicalDeviceCreation) {
+    auto i =
+        GraphRunner::Rhi::Instance("Rhi Test build", "GraphRunner", { }, { });
+    auto pdv = i.create_single_physical_device_with_best_vram( );
+    ASSERT_TRUE(pdv.has_value( ));
+}
