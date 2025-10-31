7c19035e788f1c3e54d897942e85d7942abff608
iphuck22 <eldeshue@naver.com>
2025-11-01 05:36:06 +0900

    test : erase unnecessary tests

--- tests/RhiTest.cc
+++ tests/RhiTest.cc
@@ -8,26 +8,13 @@ TEST(RhiInstance, InstanceCreation) {
     });
 }
 
-TEST(RhiPhysicalDevice, PhysicalDeviceCreation) {
+TEST(RhiObjects, ObjectCreation) {
     auto i =
         GraphRunner::Rhi::Instance("Rhi Test build", "GraphRunner", { }, { });
     auto pdv = i.create_single_physical_device_with_best_vram( );
     ASSERT_TRUE(pdv.has_value( ));
-}
-
-TEST(RhiDevice, LogicalDeviceCreation) {
-    auto i =
-        GraphRunner::Rhi::Instance("Rhi Test build", "GraphRunner", { }, { });
-    auto pdv = i.create_single_physical_device_with_best_vram( );
     auto dv = pdv.value( ).create_logical_device_with_single_graphic_queue({ });
     ASSERT_TRUE(dv.has_value( ));
-}
-
-TEST(RhiQueue, CommandQueueCreation) {
-    auto i =
-        GraphRunner::Rhi::Instance("Rhi Test build", "GraphRunner", { }, { });
-    auto pdv = i.create_single_physical_device_with_best_vram( );
-    auto dv = pdv.value( ).create_logical_device_with_single_graphic_queue({ });
     auto graphic_queue1 = dv->create_graphics_queue( );
     ASSERT_TRUE(graphic_queue1.has_value( ));
     auto graphic_queue2 = dv->create_graphics_queue( );
