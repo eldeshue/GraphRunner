8c8287adcc115a4dc28ec87e1d279dcdc29087d3
iphuck22 <eldeshue@naver.com>
2025-11-24 15:37:35 +0900

    feat : modify test
    - test create_logical_device_with_all_queues

--- tests/RhiTest.cc
+++ tests/RhiTest.cc
@@ -13,10 +13,9 @@ TEST(RhiObjects, ObjectCreation) {
         GraphRunner::Rhi::VInstance("Rhi Test build", "GraphRunner", { }, { });
     auto pdv = i.create_single_physical_device_with_best_vram( );
     ASSERT_TRUE(pdv.has_value( ));
-    auto dv = pdv.value( ).create_logical_device_with_single_graphic_queue({ });
-    ASSERT_TRUE(dv.has_value( ));
-    auto graphic_queue1 = dv->create_graphics_queue( );
+    auto dv = pdv.value( ).create_logical_device_with_all_queues({ });
+    auto graphic_queue1 = dv.create_graphics_queue( );
     ASSERT_TRUE(graphic_queue1.has_value( ));
-    auto graphic_queue2 = dv->create_graphics_queue( );
-    ASSERT_FALSE(graphic_queue2.has_value( ));
+    auto graphic_queue2 = dv.create_graphics_queue( );
+    ASSERT_TRUE(graphic_queue2.has_value( ));
 }
