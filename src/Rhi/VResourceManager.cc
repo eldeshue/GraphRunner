0630e6af552db1522ac9ddb0c4134acb1bf92387
iphuck22 <eldeshue@naver.com>
2025-11-29 19:59:15 +0900

    feat(VResourceManager) : implement deletion manager

--- src/Rhi/VResourceManager.cc
+++ src/Rhi/VResourceManager.cc
@@ -50,22 +50,139 @@ VResourceManager::VResourceManager(
     _rhi_frame_index(frame_idx),
     _rhi_device(dvc),
     _allocator(create_vma_with_volk(inst, pdvc, dvc)),
-    _semaphores(nullptr) {
-    // init
+    _render_semaphores(nullptr),
+    _del_queues( ),
+    _del_thread( ) {
+    // _del_queus are default constructed, so init device and allocator
+    for ( auto& queue : _del_queues ) {
+        // Rvalue, must be moved
+        queue = DeletionQueue(_rhi_device, _allocator);
+    }
 }
 
 VResourceManager::~VResourceManager( ) {
+    // clean up resources
+    end_render( );
     // destroy vma allocator
     if ( _allocator != VK_NULL_HANDLE ) {
         vmaDestroyAllocator(_allocator);
     }
 }
 
-void VResourceManager::set_semaphores(std::vector<VkSemaphore>* semaphores) {
-    // set before rendering start
-    _semaphores = semaphores;
+/* ---------- resource control ---------- */
+namespace {
+// helper functions
 }
 
+// start rendering
+// wait for preload ends
+// create deletion thread
+// timeline semaphores from rendering threads
+// must be called before rendering(recording commands)
+// param semaphores must not to be reallocated.
+void VResourceManager::start_render(std::vector<VkSemaphore> const* semaphores
+) {
+    // init deletion threads
+    if ( _del_thread.joinable( ) ) {
+        // end_render was not called, must not happen
+        throw std::runtime_error("Error : previous rendering was not ended.");
+    }
+
+    // TBD : wait for preloading ends
+    // waiting frame index 0 ends
+
+    // set before rendering start
+    _render_semaphores = semaphores;
+
+    // init deletion thread
+    _del_thread = std::jthread([this](std::stop_token stoken) {
+        // exception must stay in the thread
+        // or the thread will be terminated
+        try {
+            uint64_t wait_frame_index =
+                1; // frame index 0 means rendering not started
+            std::vector<uint64_t> values(this->_render_semaphores->size( ), 0);
+
+            // wait multiple timeline semaphores at the same time
+            VkSemaphoreWaitInfo waitInfo { };
+            waitInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_WAIT_INFO;
+            waitInfo.pSemaphores = this->_render_semaphores->data( );
+            waitInfo.pValues = values.data( );
+            waitInfo.semaphoreCount =
+                static_cast<uint32_t>(this->_render_semaphores->size( ));
+            waitInfo.flags = 0; // wait for all semaphores
+            waitInfo.pNext = nullptr;
+
+            // run until request_stop arrives
+            while ( !stoken.stop_requested( ) ) {
+                // set frame number to wait
+                std::fill(values.begin( ), values.end( ), wait_frame_index);
+
+                // must have time-out to check stop token
+                VkResult result = vkWaitSemaphores(
+                    this->_rhi_device,
+                    &waitInfo,
+                    100'000'000 // 100ms
+                );
+
+                if ( result == VK_SUCCESS ) {
+                    // get bucket index
+                    uint32_t bucket_index =
+                        wait_frame_index % RHI_MAX_FRAMES_IN_FLIGHT;
+
+                    // delete resources
+                    this->_del_queues[bucket_index].deque( );
+
+                    // advance to next frame-index
+                    wait_frame_index++;
+                } else if ( result == VK_TIMEOUT ) {
+                    // GPU is still busy.
+                    continue;
+                } else {
+                    // Device Lost
+                    // do not handle, panic
+                    // critical error, logging needed
+                    print_log(
+                        "Error : vkWaitSemaphores failed in deletion thread. failed result is {}.",
+                        result
+                    );
+                    break;
+                }
+            }
+        } catch ( std::exception e ) {
+            // logging
+            print_log(
+                "Error : exception thrown in deletion thread. {}.",
+                e.what( )
+            );
+        }
+    });
+}
+
+// end rendering
+// wait and delete deletion threads
+// after thread ends, delete all resources after gpu stops running
+// must be called after all rendering ends(gpu sync needed)
+void VResourceManager::end_render( ) {
+    // clear deletion manager
+    {
+        // wait for deletion thread ends
+        if ( _del_thread.joinable( ) ) {
+            _del_thread.request_stop( ); // stop_token 플래그를 true로 설정
+            _del_thread.join( ); // 스레드가 루프를 빠져나와 종료될 때까지 대기
+        }
+        // after join, _del_thread is not joinable
+
+        // after deletion thread ends, manually delete all resources
+        for ( auto& queue : _del_queues ) {
+            queue.clear_all( );
+        }
+    }
+
+    // TBD : clear staging manager
+}
+
+/* ---------- resource factory ---------- */
 namespace {
 uint64_t calc_resource_size( ) {
     // calculate size of resource with respect to alignment
--- src/Rhi/VResourceManager.h
+++ src/Rhi/VResourceManager.h
@@ -1,8 +1,12 @@
 ﻿#pragma once
 
+#include <array>
 #include <atomic>
+#include <thread>
 
+#include "./DeletionQueue.h"
 #include "./GraphicsApiCore.h"
+#include "./RhiConfig.h"
 
 namespace GraphRunner {
 namespace Rhi {
@@ -17,7 +21,7 @@ namespace Rhi {
 
         // frame index of Rhi
         // for timeline semaphore
-        std::atomic_uint64_t& _rhi_frame_index;
+        std::atomic_uint64_t const& _rhi_frame_index;
 
         // api specific
         // gpu device
@@ -30,17 +34,20 @@ namespace Rhi {
         // vector of timeline semaphores
         // each semaphore is shared by each rendering thread
         // staging, deletion will be synchronized based on those semaphores
-        std::vector<VkSemaphore>* _semaphores;
+        std::vector<VkSemaphore> const* _render_semaphores;
 
         /* ---------- dynamic resource managing ---------- */
         // deletion manager
         // per frame deletion queue
         // need mutex for each queue
         // thread for consuming data in deletion queue
+        std::array<DeletionQueue, RHI_MAX_FRAMES_IN_FLIGHT> _del_queues;
+        std::jthread _del_thread;
 
         // desc manager
         // necessary component for bindless architecture
         // mega sized descriptor array for desc_indexing
+        // TBD : after transfer
 
         // transfer manager
         // per frame large(128MB?) mapped buffer for staging(host visible, host coherent)
@@ -50,9 +57,11 @@ namespace Rhi {
         // record all copy command before rendering starts
         // synchronization needed, no ownership transfer
         // reallocate if the staging memory is not enough
+        // TBD : after deletion
 
         // streaming manager
-        // virtual tiling, TBD after RDG
+        // virtual tiling,
+        // TBD : after RDG
 
       public:
         // basic creation method
@@ -66,18 +75,33 @@ namespace Rhi {
         );
         ~VResourceManager( );
 
-        // set semaphores
-        void set_semaphores(std::vector<VkSemaphore>* semaphores);
+        /* ---------- before rendering ---------- */
+        // frame_num == 0 : before rendering
 
+        // pre_load_before_render
+        // statically loaded resources
+
+        // start rendering
+        // wait for preload ends
+        // create deletion thread
+        void start_render(std::vector<VkSemaphore> const* semaphores);
+
+        /* ---------- while rendering ---------- */
+        // frame_num > 0 : while rendering
+
+        // end rendering
+        // wait and delete deletion threads
+        // after thread ends, delete all resources after gpu stops running
+        void end_render( );
+
+        /* ---------- resource factory ---------- */
         // resource creation
+        // VBuffer abstract class
         // VVertexBuffer create_vertex_buffer()
         // VIndexBuffer create_index_buffer()
-        // VTexture2D create_texture2D();
 
-        // resource deletion
-        // enqueue by destruction of each objects
-        // dequeue(consumption) when the frame starts
-        // clear all queues, must be called after the scene ends
+        // VImage abstract class
+        // VTexture2D create_texture2D();
     };
 } // namespace Rhi
 } // namespace GraphRunner
