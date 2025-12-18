da5c429cc8e23bec72ad847659c97fd08bc9cfaa
iphuck22 <eldeshue@naver.com>
2025-12-18 20:03:27 +0900

    feat : implement VImage class

---
+++ src/Rhi/VImage.cc
@@ -0,0 +1,98 @@
+﻿
+#include "./VImage.h"
+
+#include "./VResourceManager.h"
+#include "Util.h"
+
+using namespace GraphRunner::Rhi;
+using namespace GraphRunner::Util;
+
+namespace {
+// helper functions
+
+// will be called debug only
+void set_image_debug_name(VkDevice device, VkImage handle, char const* name) {
+    VkDebugUtilsObjectNameInfoEXT nameInfo = {
+        VK_STRUCTURE_TYPE_DEBUG_UTILS_OBJECT_NAME_INFO_EXT
+    };
+    nameInfo.objectType = VK_OBJECT_TYPE_IMAGE; // object type, buffer
+    nameInfo.objectHandle = (uint64_t)(handle);
+    nameInfo.pObjectName = name;
+
+    // loaded by Volk, debug utils ext needed
+    vkSetDebugUtilsObjectNameEXT(device, &nameInfo);
+}
+
+} // namespace
+
+VImage::VImage(
+    VResourceManager& source, // factory
+    VkImageCreateInfo img_ci,
+    VkImageAspectFlags aspect_flags,
+    VmaMemoryUsage mem_usage = VMA_MEMORY_USAGE_AUTO,
+    VmaAllocationCreateFlags alloc_flags = 0,
+    VkMemoryPropertyFlags req_flags = 0,
+    VkMemoryPropertyFlags pref_flags = 0,
+    std::string_view name = ""
+) :
+    _factory(&source),
+    _cur_layout(img_ci.initialLayout),
+    _ci(img_ci),
+    _aspect_flags(aspect_flags) {
+    VmaAllocationCreateInfo alloc_ci = { };
+    alloc_ci.usage = mem_usage;
+    alloc_ci.flags = alloc_flags;
+    alloc_ci.priority = 1.0f;
+    // detailed control
+    alloc_ci.requiredFlags = req_flags; // ex) host visible
+    alloc_ci.preferredFlags = pref_flags; // ex) host coherent, host cached
+
+    check(vmaCreateImage(
+        _factory->vma_allocator( ),
+        &img_ci,
+        &alloc_ci,
+        &_handle,
+        &_alloc,
+        &_alloc_info
+    ));
+
+// for debugging, set name of the object
+#ifdef ENABLE_VULKAN_VALIDATION
+    set_image_debug_name(_factory->device( ), _handle, name.data( ));
+#endif
+}
+
+VImage::~VImage( ) {
+    // push resources to the deletion queue
+    // allocation and buffer
+    if ( _alloc != nullptr ) {
+        _factory->get_current_deletion_queue( ).enque(
+            std::make_pair(_alloc, _handle)
+        );
+    }
+}
+
+VImage::VImage(VImage&& other) noexcept :
+    _factory(other._factory),
+    _handle(other._handle),
+    _alloc(other._alloc),
+    _alloc_info(other._alloc_info),
+    _cur_layout(other._cur_layout),
+    _ci(other._ci),
+    _aspect_flags(other._aspect_flags) {
+    other._alloc = nullptr;
+}
+
+VImage& VImage::operator=(VImage&& other) noexcept {
+    if ( this != &other ) {
+        // swap
+        std::swap(_factory, other._factory);
+        std::swap(_handle, other._handle);
+        std::swap(_alloc, other._alloc);
+        std::swap(_alloc_info, other._alloc_info);
+        std::swap(_cur_layout, other._cur_layout);
+        std::swap(_ci, other._ci);
+        std::swap(_aspect_flags, other._aspect_flags);
+    }
+    return *this;
+}
---
+++ src/Rhi/VImage.h
@@ -0,0 +1,103 @@
+﻿
+#pragma once
+
+#include <string_view>
+
+#include "./GraphicsApiCore.h"
+#include "./VResource.h"
+
+namespace GraphRunner {
+namespace Rhi {
+    class VResourceManager;
+
+    class VImage: public VResource {
+      private:
+        // no default, no copy
+        VImage( ) = delete;
+        VImage(VImage const&) = delete;
+        VImage& operator=(VImage const&) = delete;
+
+      protected:
+        // resource manager is the factory
+        // friend class VResourceManager;
+        // factory is always has to be single
+        VResourceManager* _factory;
+
+        // vulkan image handle
+        VkImage _handle = VK_NULL_HANDLE;
+
+        // vma related
+        VmaAllocation _alloc = nullptr;
+        VmaAllocationInfo _alloc_info = { };
+
+        // vulkan related status
+        VkImageLayout _cur_layout = VK_IMAGE_LAYOUT_UNDEFINED;
+        VkImageCreateInfo _ci;
+        VkImageAspectFlags _aspect_flags;
+
+      public:
+        // basic constructr
+        VImage(
+            VResourceManager& source, // factory
+            VkImageCreateInfo ci,
+            VkImageAspectFlags aspect_flags,
+            VmaMemoryUsage mem_usage = VMA_MEMORY_USAGE_AUTO,
+            VmaAllocationCreateFlags alloc_flags = 0,
+            VkMemoryPropertyFlags req_flags = 0,
+            VkMemoryPropertyFlags pref_flags = 0,
+            std::string_view name = ""
+        );
+        // destructor
+        virtual ~VImage( );
+        // move
+        VImage(VImage&& other) noexcept;
+        VImage& operator=(VImage&& other) noexcept;
+
+        /* ---------  Getter --------- */
+        // vulkan image handle
+        VkImage handle( ) const {
+            return _handle;
+        }
+
+        // vulkan related status
+        VkImageLayout cur_layout( ) const {
+            return _cur_layout;
+        }
+
+        VkFormat format( ) const {
+            return _ci.format;
+        }
+
+        VkImageUsageFlags usage_flags( ) const {
+            return _ci.usage;
+        }
+
+        VkImageAspectFlags aspect_flags( ) const {
+            return _aspect_flags;
+        }
+
+        // other meta data
+        VkExtent3D extent( ) const {
+            return _ci.extent;
+        }
+
+        uint32_t mip_level_cnt( ) const {
+            return _ci.mipLevels;
+        }
+
+        uint32_t array_layer_cnt( ) const {
+            return _ci.arrayLayers;
+        }
+
+        size_t size( ) const {
+            return _alloc_info.size;
+        }
+
+        // set current layout status
+        // actual layout transfer needs command recording
+        void set_new_layout(VkImageLayout new_layout) {
+            _cur_layout = new_layout;
+        }
+    };
+} // namespace Rhi
+} // namespace GraphRunner
