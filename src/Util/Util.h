94094be35bded48640c460a1e977888888b315a1
iphuck22 <eldeshue@naver.com>
2025-12-22 00:12:11 +0900

    feat : implement spin lock, reference from cppreference

--- src/Util/Util.h
+++ src/Util/Util.h
@@ -22,6 +22,33 @@ namespace Util {
 
     constexpr VkDeviceSize
     calculate_aligned_value(VkDeviceSize target_value, VkDeviceSize alignment);
+
+    // simple implementation of spin lock, minimize context switching
+    // implementation is from atomic_flag in cppreference
+    class SpinLock {
+      private:
+        std::atomic_flag _mutex_flag { };
+
+      public:
+        void lock( ) noexcept {
+            // if flag is clear, set and return false(prev value)
+            // if flag is already set, return true(prev value)
+            while ( _mutex_flag.test_and_set(std::memory_order_acquire) ) {
+                // hybrid spin lock, effective busy waiting
+                // wake up by notify one in unlock
+                _mutex_flag.wait(true, std::memory_order_relaxed);
+            }
+        }
+
+        bool try_lock( ) noexcept {
+            return !_mutex_flag.test_and_set(std::memory_order_acquire);
+        }
+
+        void unlock( ) noexcept {
+            _mutex_flag.clear(std::memory_order_release);
+            _mutex_flag.notify_one( ); // signal one waiting thread
+        }
+    };
 } // namespace Util
 } // namespace GraphRunner
 
